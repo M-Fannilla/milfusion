de1cd62178394be62107056cfb4b66d0bb301702
Miłosz Bertman <milosz.bertman@gmail.com>
2024-05-29 08:54:53 +0000

    Multiprocess download

--- download_from_gcp.py
+++ download_from_gcp.py
@@ -3,7 +3,10 @@ import concurrent
 from tqdm import tqdm
 from pathlib import Path
 from google.cloud import storage
-from concurrent.futures import ThreadPoolExecutor
+import multiprocessing
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from tqdm import tqdm
+import concurrent.futures
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/teamspace/uploads/fannilla-cfd25ffd1b7c.json"
 
@@ -24,36 +27,37 @@ CATEGORIES = [
 BUCKET_NAME = 'pornpics_scrape'
 
 
+
 def fetch_all_blobs(folder_prefix: str = None):
     """Fetch all file paths from the GCS bucket."""
     blobs = BUCKET.list_blobs(prefix=folder_prefix)
     return [blob.name for blob in blobs]
 
-
-def download_category(category_blobs, destination_folder, force_download):
+def download_blob(b, destination_folder, force_download, pbar):
     """Download a blob from the GCS bucket."""
+    file_path = Path(destination_folder) / b
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    if not force_download and file_path.exists():
+        pbar.update(1)
+        return
+    BUCKET.blob(b).download_to_filename(file_path.as_posix())
+    pbar.update(1)
 
-    def _download_blob(b):
-        file_path = Path(destination_folder) / b
-        file_path.parent.mkdir(parents=True, exist_ok=True)
-        if not force_download and file_path.exists():
-            return
-        BUCKET.blob(b).download_to_filename(file_path.as_posix())
-
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(_download_blob, b) for b in category_blobs]
-        for _ in tqdm(
-            concurrent.futures.as_completed(futures), 
-            total=len(futures), 
-            desc=f"Downloading Blobs {category_blobs[0].split('/')[0]}"
-            ):
-            pass
+def download_category(category_blobs, destination_folder, force_download, pbar_position):
+    """Download a category of blobs from the GCS bucket."""
+    with tqdm(total=len(category_blobs), desc=f"Category {category_blobs[0].split('/')[0]}", position=pbar_position) as pbar:
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(download_blob, b, destination_folder, force_download, pbar) for b in category_blobs]
+            for future in concurrent.futures.as_completed(futures):
+                future.result()  # To raise any exceptions
 
 if __name__ == "__main__":
-    for category in CATEGORIES:
-        category_blobs = fetch_all_blobs(folder_prefix=category)
-        download_category(
-            category_blobs=category_blobs,
-            destination_folder=DESTINATION_FOLDER,
-            force_download=False
-        )
+    manager = multiprocessing.Manager()
+    with ProcessPoolExecutor(max_workers=32) as executor:
+        futures = [
+            executor.submit(download_category, fetch_all_blobs(folder_prefix=category), DESTINATION_FOLDER, False, i) 
+            for i, category in enumerate(CATEGORIES)
+        ]
+        
+        for _ in concurrent.futures.as_completed(futures):
+            pass
