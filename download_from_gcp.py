7b114abb0883575757f65fd44660774d3c0d1244
Milosz <milosz@fannilla.com>
2024-05-30 12:31:48 +0200

    cleanup

--- download_from_gcp.py
+++ download_from_gcp.py
@@ -8,7 +8,7 @@ import concurrent.futures
 from google.cloud import storage
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/teamspace/uploads/fannilla-cfd25ffd1b7c.json"
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/teamspace/uploads/fannilla-cfd25ffd1b7c.json"
 
 BUCKET_NAME = 'pornpics_scrape'
 CLIENT = storage.Client()
@@ -16,15 +16,23 @@ BUCKET = CLIENT.bucket(BUCKET_NAME)
 DESTINATION_FOLDER = "./images"
 
 CATEGORIES = [
-    'amateur', 'bath', 'big-tits', 'bikini', 'blonde', 'boots', 'brunette', 'centerfold', 'christmas',
-    'chubby', 'clothed', 'cougar', 'curvy', 'doggystyle', 'face', 'fake-tits', 'feet', 'glasses', 'granny',
-    'hairy', 'high-heels', 'homemade', 'housewife', 'jeans', 'legs', 'lingerie', 'maid', 'masturbation', 'mature',
-    'milf', 'model', 'mom', 'natural-tits', 'non-nude', 'nurse', 'office', 'panties', 'pantyhose', 'pool',
-    'pornstar', 'redhead', 'saggy-tits', 'secretary', 'selfie', 'sexy', 'shaved', 'short-hair', 'shorts', 'skinny',
-    'skirt', 'smoking', 'socks', 'solo', 'spreading', 'stockings', 'thick', 'thong', 'undressing', 'upskirt', 'white',
-    'asian', 'ass', 'babe', 'beautiful', 'bondage', 'brazilian', 'college', 'cosplay', 'ebony', 'european',
-    'girlfriend', 'glamour', 'japanese', 'latex', 'latina', 'nipples', 'nude', 'oiled', 'sports', 'tattoo', 'teen',
-    'thai', 'uniform', 'yoga-pants'
+    'amateur', 'asian', 'ass', 'babe', 'bath', 'beautiful', 'big-tits', 'bikini', 'blonde', 'bondage', 'boots',
+    'brazilian', 'brunette', 'centerfold', 'christmas', 'chubby', 'clothed', 'college', 'cosplay', 'cougar', 'curvy',
+    'doggystyle', 'ebony', 'european', 'face', 'fake-tits', 'feet', 'girlfriend', 'glamour', 'glasses', 'granny',
+    'hairy', 'high-heels', 'homemade', 'housewife', 'japanese', 'jeans', 'latex', 'latina', 'legs', 'lingerie', 'maid',
+    'masturbation', 'mature', 'milf', 'model', 'mom', 'natural-tits', 'nipples', 'non-nude', 'nurse', 'office',
+    'oiled', 'panties', 'pantyhose', 'pool', 'pornstar', 'redhead', 'saggy-tits', 'secretary', 'selfie', 'sexy',
+    'shaved', 'short-hair', 'shorts', 'skinny', 'skirt', 'smoking', 'socks', 'solo', 'sports', 'spreading', 'stockings',
+    'tattoo', 'teen', 'thai', 'thick', 'thong', 'undressing', 'uniform', 'upskirt', 'white', 'yoga-pants'
+]
+
+MAC_CAT = [
+    'amateur', 'non-nude', 'bondage', 'boots', 'christmas', 'chubby', 'clothed', 'curvy',
+    'doggystyle', 'glasses', 'granny',
+    'high-heels', 'homemade', 'housewife', 'jeans', 'lingerie', 'maid',
+    'mature', 'milf', 'natural-tits', 'nurse', 'office',
+    'saggy-tits', 'selfie', 'smoking', 'socks', 'solo', 'sports', 'spreading', 'stockings',
+    'tattoo', 'teen', 'thick', 'undressing', 'uniform', 'upskirt', 'white', 'yoga-pants'
 ]
 
 BUCKET_NAME = 'pornpics_scrape'
@@ -49,8 +57,9 @@ def download_blob(b, destination_folder, force_download, pbar):
 
 def download_category(category_blobs, destination_folder, force_download, pbar_position):
     """Download a category of blobs from the GCS bucket."""
-    with tqdm(total=len(category_blobs), desc=f"Category {category_blobs[0].split('/')[0]}",
-              position=pbar_position) as pbar:
+    with tqdm(
+            total=len(category_blobs), desc=f"Category {category_blobs[0].split('/')[0]}", position=pbar_position
+    ) as pbar:
         with ThreadPoolExecutor() as executor:
             futures = [executor.submit(download_blob, b, destination_folder, force_download, pbar) for b in
                        category_blobs]
@@ -60,10 +69,10 @@ def download_category(category_blobs, destination_folder, force_download, pbar_p
 
 if __name__ == "__main__":
     manager = multiprocessing.Manager()
-    with ProcessPoolExecutor(max_workers=32) as executor:
+    with ProcessPoolExecutor(max_workers=multiprocessing.cpu_count() - 1) as executor:
         futures = [
             executor.submit(download_category, fetch_all_blobs(folder_prefix=category), DESTINATION_FOLDER, False, i)
-            for i, category in enumerate(CATEGORIES)
+            for i, category in enumerate(MAC_CAT)
         ]
 
         for _ in concurrent.futures.as_completed(futures):
