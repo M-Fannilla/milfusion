c1b7f8959ea401f929e5379c63d2855d970244db
Milosz <milosz@fannilla.com>
2024-06-10 17:48:29 +0200

    before balancing changes

--- download_dataset_from_gcp.py
+++ download_dataset_from_gcp.py
@@ -1,21 +1,15 @@
 import pandas as pd
 from tqdm import tqdm
-from pathlib import Path
-from google.cloud import storage
+import multiprocessing
+from utils import SRC_DIR, BUCKET
 from concurrent.futures import ThreadPoolExecutor
 
-BUCKET_NAME = 'chum_bucket_stuff'
-CLIENT = storage.Client()
-
-BUCKET = CLIENT.bucket(BUCKET_NAME)
-DESTINATION_FOLDER = Path("./images")
-
-filenames = pd.read_csv("datasets/ai_gen_dataset_10_cats.csv")["file_path"].tolist()
+filenames = pd.read_csv("datasets/cropped_medium_one_hot.csv")["file_path"].tolist()
 
 
 def main_process(file_name: str):
     gcp_file_path = "pics/" + file_name
-    destination = DESTINATION_FOLDER / file_name
+    destination = SRC_DIR / file_name
     if destination.exists():
         return
     destination.parent.mkdir(parents=True, exist_ok=True)
@@ -23,7 +17,7 @@ def main_process(file_name: str):
 
 
 if __name__ == "__main__":
-    with ThreadPoolExecutor(max_workers=32) as executor:
+    with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count() * 2) as executor:
         results = [
             executor.submit(main_process, filename)
             for filename in filenames
