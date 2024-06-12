28dfd8e122d4f6f96f2933389de370193baf1931
Milosz <milosz@fannilla.com>
2024-06-12 11:37:40 +0200

    before balancing changes

---
+++ crop_datasets.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from pathlib import Path
+from utils import SRC_DIR
+
+
+def is_cropped(row):
+    file_path = Path(row.file_path)
+    cropped_file_path = SRC_DIR / file_path.parent / 'cropped' / file_path.name
+    if cropped_file_path.exists():
+        return True
+    else:
+        return False
+
+
+def replace_file_path(row):
+    file_path = Path(row.file_path)
+    return (file_path.parent / 'cropped' / file_path.name).as_posix()
+
+
+if __name__ == "__main__":
+    for dataset_path in [
+        'ai_gen',
+        'medium_one_hot',
+        'small_one_hot'
+    ]:
+        df = pd.read_csv(f'datasets/{dataset_path}.csv', index_col=0)
+        start = df.shape[0]
+        df['is_cropped'] = df.apply(is_cropped, axis=1)
+        df = df[df['is_cropped']]
+        df['file_path'] = df.apply(replace_file_path, axis=1)
+        df.drop(['is_cropped'], axis=1, inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        df.to_csv(f"datasets/cropped_{dataset_path}.csv")
+        print(dataset_path, "reduced by", abs(df.shape[0] - start))
