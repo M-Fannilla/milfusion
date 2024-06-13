ef6b9b8b02c059c8a1d4b88de3b4d0b477e84a57
Milosz <milosz@fannilla.com>
2024-06-13 10:59:34 +0200

    Added dataset compiler

--- compile_dataset.py
+++ compile_dataset.py
@@ -1,4 +1,5 @@
 import shutil
+from pathlib import Path
 
 import pandas as pd
 from tqdm import tqdm
@@ -9,11 +10,12 @@ from concurrent.futures import ThreadPoolExecutor
 dataset_name = "cropped_medium_one_hot"
 df = pd.read_csv(f"datasets/{dataset_name}.csv", index_col=0)
 filenames = df["file_path"].tolist()
+IMAGE_DIR = Path("./images")
 
 
 def main_process(file_name: str, preserve_structure: bool = False):
     gcp_file_path = "pics/" + file_name
-    local_destination = SRC_DIR / file_name if preserve_structure else SRC_DIR / file_name.split("/")[-1]
+    local_destination = IMAGE_DIR / file_name if preserve_structure else IMAGE_DIR / file_name.split("/")[-1]
 
     if not local_destination.exists():
         local_destination.parent.mkdir(parents=True, exist_ok=True)
@@ -22,7 +24,7 @@ def main_process(file_name: str, preserve_structure: bool = False):
 
 def main_process_from_drive(file_name: str, preserve_structure: bool = False):
     source_path = SRC_DIR / file_name
-    target_path = SRC_DIR / file_name if preserve_structure else SRC_DIR / file_name.split("/")[-1]
+    target_path = IMAGE_DIR / file_name if preserve_structure else IMAGE_DIR / file_name.split("/")[-1]
     target_path.parent.mkdir(parents=True, exist_ok=True)
     shutil.copy(source_path, target_path)
 
